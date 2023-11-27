23eb5ec8462dd4a6c8596284e650642349c0e16d
Olga Vlasenco <olga.vlasenco@ellation.com>
2023-11-27 17:25:50 +0200

    adding API gateway to  test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 
-url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
-
+#url = 'http://localhost:8080/2015-03-31/functions/function/invocations' #local host link
+url = 'https://rzi7zlm2p0.execute-api.us-west-2.amazonaws.com/test/predict'
 data = {'url': 'https://habrastorage.org/webt/rt/d9/dh/rtd9dhsmhwrdezeldzoqgijdg8a.jpeg'}
 
 result = requests.post(url, json=data).json()
